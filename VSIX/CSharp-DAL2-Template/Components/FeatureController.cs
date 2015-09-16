2f462876467091d929a0442264e137803d995e80
Will Strohl <will.strohl@gmail.com>
2015-09-15 22:58:11 -0700

    Updated C# DAL 2 module feature controller with implementations

--- VSIX/CSharp-DAL2-Template/Components/FeatureController.cs
+++ VSIX/CSharp-DAL2-Template/Components/FeatureController.cs
@@ -10,14 +10,18 @@
 ' 
 */
 
+using System;
 using System.Collections.Generic;
-//using System.Xml;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using DotNetNuke.Common.Utilities;
 using DotNetNuke.Entities.Modules;
-using DotNetNuke.Services.Search;
+using DotNetNuke.Services.Search.Entities;
 
 namespace $rootnamespace$$safeprojectname$.Components
 {
-
     /// -----------------------------------------------------------------------------
     /// <summary>
     /// The Controller class for $safeprojectname$
@@ -35,103 +39,159 @@ namespace $rootnamespace$$safeprojectname$.Components
     /// Below you will find stubbed out implementations of each, uncomment and populate with your own data
     /// </summary>
     /// -----------------------------------------------------------------------------
-
-    //uncomment the interfaces to add the support.
-    public class FeatureController //: IPortable, ISearchable, IUpgradeable
+    public class FeatureController : ModuleSearchBase, IPortable, IUpgradeable
     {
-
+        // feel free to remove any interfaces that you don't wish to use
+        // (requires that you also update the .dnn manifest file)
 
         #region Optional Interfaces
+
+        /// <summary>
+        /// Gets the modified search documents for the DNN search engine indexer.
+        /// </summary>
+        /// <param name="moduleInfo">The module information.</param>
+        /// <param name="beginDate">The begin date.</param>
+        /// <returns></returns>
+        public override IList<SearchDocument> GetModifiedSearchDocuments(ModuleInfo moduleInfo, DateTime beginDate)
+        {
+            var searchDocuments = new List<SearchDocument>();
+            var controller = new ItemController();
+            var items = controller.GetItems(moduleInfo.ModuleID);
+
+            foreach (var item in items)
+            {
+                if (item.LastModifiedOnDate.ToUniversalTime() <= beginDate.ToUniversalTime() ||
+                    item.LastModifiedOnDate.ToUniversalTime() >= DateTime.UtcNow)
+                    continue;
+
+                var content = string.Format("{0}<br />{1}", item.ItemName, item.ItemDescription);
+
+                var searchDocumnet = new SearchDocument
+                {
+                    UniqueKey = string.Format("Items:{0}:{1}", moduleInfo.ModuleID, item.ItemId),  // any unique identifier to be able to query for your individual record
+                    PortalId = moduleInfo.PortalID,  // the PortalID
+                    TabId = moduleInfo.TabID, // the TabID
+                    AuthorUserId = item.LastModifiedByUserId, // the person who created the content
+                    Title = moduleInfo.ModuleTitle,  // the title of the content, but should be the module title
+                    Description = moduleInfo.DesktopModule.Description,  // the description or summary of the content
+                    Body = content,  // the long form of your content
+                    ModifiedTimeUtc = item.LastModifiedOnDate.ToUniversalTime(),  // a time stamp for the search results page
+                    CultureCode = moduleInfo.CultureCode, // the current culture code
+                    IsActive = true  // allows you to remove the item from the search index (great for soft deletes)
+                };
+
+                searchDocuments.Add(searchDocumnet);
+            }
+
+            return searchDocuments;
+        }
 
         /// -----------------------------------------------------------------------------
         /// <summary>
         /// ExportModule implements the IPortable ExportModule Interface
         /// </summary>
-        /// <param name="ModuleID">The Id of the module to be exported</param>
+        /// <param name="moduleId">The Id of the module to be exported</param>
         /// -----------------------------------------------------------------------------
-        //public string ExportModule(int ModuleID)
-        //{
-        //string strXML = "";
+        public string ExportModule(int moduleId)
+        {
+            var controller = new ItemController();
+            var items = controller.GetItems(moduleId);
+            var sb = new StringBuilder();
 
-        //List<$safeprojectname$Info> col$safeprojectname$s = Get$safeprojectname$s(ModuleID);
-        //if (col$safeprojectname$s.Count != 0)
-        //{
-        //    strXML += "<$safeprojectname$s>";
+            var itemList = items as IList<Item> ?? items.ToList();
 
-        //    foreach ($safeprojectname$Info obj$safeprojectname$ in col$safeprojectname$s)
-        //    {
-        //        strXML += "<$safeprojectname$>";
-        //        strXML += "<content>" + DotNetNuke.Common.Utilities.XmlUtils.XMLEncode(obj$safeprojectname$.Content) + "</content>";
-        //        strXML += "</$safeprojectname$>";
-        //    }
-        //    strXML += "</$safeprojectname$s>";
-        //}
+            if (!itemList.Any()) return string.Empty;
 
-        //return strXML;
+            sb.Append("<Items>");
 
-        //	throw new System.NotImplementedException("The method or operation is not implemented.");
-        //}
+            foreach (Item item in itemList)
+            {
+                sb.Append("<Item>");
+
+                sb.AppendFormat("<AssignedUserId>{0}</AssignedUserId>", item.AssignedUserId);
+                sb.AppendFormat("<CreatedByUserId>{0}</CreatedByUserId>", item.CreatedByUserId);
+                sb.AppendFormat("<CreatedOnDate>{0}</CreatedOnDate>", item.CreatedOnDate);
+                sb.AppendFormat("<ItemId>{0}</ItemId>", item.ItemId);
+                sb.AppendFormat("<ItemDescription>{0}</ItemDescription>", XmlUtils.XMLEncode(item.ItemDescription));
+                sb.AppendFormat("<ItemName>{0}</ItemName>", XmlUtils.XMLEncode(item.ItemName));
+                sb.AppendFormat("<LastModifiedByUserId>{0}</LastModifiedByUserId>", item.LastModifiedByUserId);
+                sb.AppendFormat("<LastModifiedOnDate>{0}</LastModifiedOnDate>", item.LastModifiedOnDate);
+                sb.AppendFormat("<ModuleId>{0}</ModuleId>", item.ModuleId);
+
+                sb.Append("</Item>");
+            }
+
+            sb.Append("</Items>");
+
+            // you might consider doing something similar here for any important module settings
+
+            return sb.ToString();
+        }
 
         /// -----------------------------------------------------------------------------
         /// <summary>
         /// ImportModule implements the IPortable ImportModule Interface
         /// </summary>
-        /// <param name="ModuleID">The Id of the module to be imported</param>
-        /// <param name="Content">The content to be imported</param>
-        /// <param name="Version">The version of the module to be imported</param>
-        /// <param name="UserId">The Id of the user performing the import</param>
+        /// <param name="moduleId">The Id of the module to be imported</param>
+        /// <param name="content">The content to be imported</param>
+        /// <param name="version">The version of the module to be imported</param>
+        /// <param name="userId">The Id of the user performing the import</param>
         /// -----------------------------------------------------------------------------
-        //public void ImportModule(int ModuleID, string Content, string Version, int UserID)
-        //{
-        //XmlNode xml$safeprojectname$s = DotNetNuke.Common.Globals.GetContent(Content, "$safeprojectname$s");
-        //foreach (XmlNode xml$safeprojectname$ in xml$safeprojectname$s.SelectNodes("$safeprojectname$"))
-        //{
-        //    $safeprojectname$Info obj$safeprojectname$ = new $safeprojectname$Info();
-        //    obj$safeprojectname$.ModuleId = ModuleID;
-        //    obj$safeprojectname$.Content = xml$safeprojectname$.SelectSingleNode("content").InnerText;
-        //    obj$safeprojectname$.CreatedByUser = UserID;
-        //    Add$safeprojectname$(obj$safeprojectname$);
-        //}
+        public void ImportModule(int moduleId, string content, string version, int userId)
+        {
+            var controller = new ItemController();
+            var items = DotNetNuke.Common.Globals.GetContent(content, "Items");
+            var xmlNodeList = items.SelectNodes("Item");
 
-        //	throw new System.NotImplementedException("The method or operation is not implemented.");
-        //}
+            if (xmlNodeList == null) return;
 
-        /// -----------------------------------------------------------------------------
-        /// <summary>
-        /// GetSearchItems implements the ISearchable Interface
-        /// </summary>
-        /// <param name="ModInfo">The ModuleInfo for the module to be Indexed</param>
-        /// -----------------------------------------------------------------------------
-        //public DotNetNuke.Services.Search.SearchItemInfoCollection GetSearchItems(DotNetNuke.Entities.Modules.ModuleInfo ModInfo)
-        //{
-        //SearchItemInfoCollection SearchItemCollection = new SearchItemInfoCollection();
+            foreach (XmlNode item in xmlNodeList)
+            {
+                var newItem = new Item()
+                {
+                    ModuleId = moduleId,
+                    // assigning everything to the current UserID, because this might be a new DNN installation
+                    // your use case might be different though
+                    CreatedByUserId = userId,
+                    LastModifiedByUserId = userId,
+                    CreatedOnDate = DateTime.Now,
+                    LastModifiedOnDate = DateTime.Now
+                };
 
-        //List<$safeprojectname$Info> col$safeprojectname$s = Get$safeprojectname$s(ModInfo.ModuleID);
+                // NOTE: If moving from one installation to another, this user will not exist
+                newItem.AssignedUserId = int.Parse(item.SelectSingleNode("AssignedUserId").InnerText, NumberStyles.Integer);
+                newItem.ItemDescription = item.SelectSingleNode("ItemDescription").InnerText;
+                newItem.ItemName = item.SelectSingleNode("ItemName").InnerText;
 
-        //foreach ($safeprojectname$Info obj$safeprojectname$ in col$safeprojectname$s)
-        //{
-        //    SearchItemInfo SearchItem = new SearchItemInfo(ModInfo.ModuleTitle, obj$safeprojectname$.Content, obj$safeprojectname$.CreatedByUser, obj$safeprojectname$.CreatedDate, ModInfo.ModuleID, obj$safeprojectname$.ItemId.ToString(), obj$safeprojectname$.Content, "ItemId=" + obj$safeprojectname$.ItemId.ToString());
-        //    SearchItemCollection.Add(SearchItem);
-        //}
-
-        //return SearchItemCollection;
-
-        //	throw new System.NotImplementedException("The method or operation is not implemented.");
-        //}
+                controller.CreateItem(newItem);
+            }
+        }
 
         /// -----------------------------------------------------------------------------
         /// <summary>
         /// UpgradeModule implements the IUpgradeable Interface
         /// </summary>
-        /// <param name="Version">The current version of the module</param>
+        /// <param name="version">The current version of the module</param>
         /// -----------------------------------------------------------------------------
-        //public string UpgradeModule(string Version)
-        //{
-        //	throw new System.NotImplementedException("The method or operation is not implemented.");
-        //}
+        public string UpgradeModule(string version)
+        {
+            try
+            {
+                switch (version)
+                {
+                    case "00.00.01":
+                        // run your custom code here
+                        return "success";
+                    default:
+                        return "success";
+                }
+            }
+            catch
+            {
+                return "failure";
+            }
+        }
 
         #endregion
-
     }
-
 }
