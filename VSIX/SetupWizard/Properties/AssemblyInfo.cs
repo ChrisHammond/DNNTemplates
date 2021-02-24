710ec973ea6d8b663f116df7d01aef6ea497eb88
Chris Hammond <christoc@live.com>
2021-02-24 12:18:37 -0600

    missed assembly info

--- VSIX/SetupWizard/Properties/AssemblyInfo.cs
+++ VSIX/SetupWizard/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Christoc.com Software Solutions")]
 [assembly: AssemblyProduct("SetupWizard")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
