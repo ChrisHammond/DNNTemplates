fb1cbceddf46262d0d0b6f68c6727dbeded79e6e
Chris Hammond <github@christoc.com>
2023-06-12 16:39:44 -0500

    Changes for 2023

--- VSIX/SetupWizard/Properties/AssemblyInfo.cs
+++ VSIX/SetupWizard/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Christoc.com Software Solutions")]
 [assembly: AssemblyProduct("SetupWizard")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyCopyright("Copyright © 2010-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.1.0.0")]
-[assembly: AssemblyFileVersion("11.1.0.0")]
+[assembly: AssemblyVersion("11.2.0.0")]
+[assembly: AssemblyFileVersion("11.2.0.0")]
